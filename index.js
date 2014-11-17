79b59821d9e0651db02cd06422657a213164ef02
Rich Trott <rtrott@gmail.com>
2014-11-16 23:24:55 -0800

    clean up this mess

--- index.js
+++ index.js
@@ -17,6 +17,33 @@ var callbackify = function (callback) {
   };
 };
 
+var callbackify2 = function (callback) {
+  return function (data) {
+    if (!data) {
+      callback(new Error("unknown error"));
+    }
+    if (data.result instanceof Array) {
+      var result = data.result.map(function (value) {
+        if (value.track_contributions instanceof Array) {
+          return value.track_contributions.map(function (value) {
+            if (value.track) {
+              return value.track.mid;
+            }
+          });
+        }
+      });
+      result = [].concat.apply([], result);
+      callback(null, result);
+    } else {
+      callback(data);
+    }
+  };
+};
+
+var options = {
+  html_escape: false
+};
+
 exports.getArtistMids = function (name, callback) {
   var query = [{
     mid: null,
@@ -24,9 +51,32 @@ exports.getArtistMids = function (name, callback) {
     type: "/music/artist"
   }];
 
-  var options = {
-    html_escape: false,
-  };
+  var myCallback = callbackify(callback);
+  freebase.mqlread(query, options, myCallback);
+};
+
+exports.getTracksWithContributors = function (mids, callback) {
+  var query = [{
+    "mid|=": mids,
+    "type": "/music/artist",
+    "track_contributions": [{
+      "track": {
+        mid: null
+      },
+      limit: 9007199254740992
+    }],
+  }];
+
+  var myCallback = callbackify2(callback);
+  freebase.mqlread(query, options, myCallback);
+};
+
+exports.getTracksByArtists = function (mids, callback) {
+  var query = [{
+    mid: null,
+    type: "/music/track",
+    "artist|=": mids
+  }];
 
   var myCallback = callbackify(callback);
   freebase.mqlread(query, options, myCallback);
--- test/index.js
+++ test/index.js
@@ -16,11 +16,24 @@ describe('exports', function () {
 	describe('getArtistMids()', function () {
 		it('should retrieve MIDs for all artists with the supplied name', function (done) {
 			var callback = function (err, data) {
+				expect(err).to.be.null();
 				expect(data).to.contain('/m/0160yj');
 				done();
 			};
 
 			routes.getArtistMids('Magma', callback);
+		});
+	});
+
+	describe('getTracksWithContributors()', function () {
+		it('should retrieve tracks with any of the supplied contributors', function (done) {
+			var callback = function (err, data) {
+				expect(err).to.be.null();
+				expect(data).to.contain('/m/0mlx6x');
+				done();
+			};
+
+			routes.getTracksWithContributors(['/m/03j24kf'], callback);
 		});
 	});
 });
